5eb885dbe8cbcc82453b5ad8a727cc6e567d6d1c
kodadicodes <128705604+kodadicodes@users.noreply.github.com>
2023-05-28 19:58:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const username = "mahmut";
+const password = "1234567";
+
+/* if (username.length > 6) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if (password.length > 5) {
+  console.log("true");
+} else {
+  console.log("false");
+} */
+
+//! KISA YOLU
+username.length > 6 ? console.log("true") : console.log("false");
+password.length > 5 ? console.log("true") : console.log("false");
